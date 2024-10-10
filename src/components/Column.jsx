9bd7c45f2a7c277f079face4d8eaed6731100dd7
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-10-10 16:08:55 +0200

    Tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-
+import Task from "./Task";
 function Column({ title }) {
   return (
-    <div className="w-[300px] h-[400px] bg-gray-200 p-4 flex flex-col border items-center">
-      <h3 className="font-bold mb-4">{title}</h3>
+    <div className="w-[300px] h-[400px] bg-gray-200 p-10 flex flex-col  items-center shadow-2xl border-2 rounded-xl border-orange-400">
+      <div className=" flex w-full justify-between items-center ">
+        <h3 className="font-bold mb-4 border border-b-orange-600 w-full  text-center">
+          {title}
+        </h3>
+        <button className="right-0">Add</button>
+      </div>
       {/* Add your tasks here */}
+      <div className="flex flex-col justify-evenly items-center h-full w-full">
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </div>
     </div>
   );
 }
---
+++ src/components/Task.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Task() {
+  return (
+    <div className="w-full px-5  bg-orange-400 border border-black">
+      Im a task
+    </div>
+  );
+}
+
+export default Task;
